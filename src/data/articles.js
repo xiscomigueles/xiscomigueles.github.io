84514f07a0a944a4296f1bacfe584b6f45eaebed
xiscomigueles <miguelesdominguez@gmail.com>
2025-08-15 11:27:44 +0200

    Experiencia laboral 1
    Primera introduccion a la experiencia laboral

--- src/data/articles.js
+++ src/data/articles.js
@@ -2,11 +2,11 @@ import React from "react";
 
 function article_1() {
 	return {
-		logo: "../cambridge.png",
-		date: "7 May 2023",
-		title: "The Benefits of Cloud Computing",
+		logo: "../grupoSolutia.png",
+		date: "9/11/2023 - Actualidad",
+		title: "Programador Senior [Grupo Solutia]",
 		description:
-			"Cloud computing offers a range of benefits, including cost savings and increased flexibility. Find out why more businesses are turning to the cloud.",
+			"Programador Full Stack con experiencia en SQL, Java y desarrollo front-end con Web Components (Lit + Vite), participando en proyectos para el SAS y Mapfre bajo metodologías ágiles.",
 		keywords: [
 			"The Benefits of Cloud Computing",
 			"Tharindu",
@@ -14,27 +14,10 @@ function article_1() {
 			"Tharindu Nayanajith",
 		],
 		style: `
-				.article-content {
-					display: flex;
-					flex-direction: column;
-					align-items: center;
-				}
-
-				.randImage {
-					align-self: center;
-					outline: 2px solid red;
-				}
 				`,
 		body: (
 			<React.Fragment>
-				<div className="article-content">
-					<div className="paragraph">Content of article 1</div>
-					<img
-						src="https://picsum.photos/200/300"
-						alt="random"
-						className="randImage"
-					/>
-				</div>
+				<h1>HOLA???? of article 2</h1>
 			</React.Fragment>
 		),
 	};
@@ -42,11 +25,11 @@ function article_1() {
 
 function article_2() {
 	return {
-		logo: "../cambridge.png",
-		date: "7 May 2023",
-		title: "Artificial Intelligence in Healthcare",
+		logo: "../nttdata.png",
+		date: "02/03/2023 - 01/07/2023",
+		title: "Programador de Prácticas [NTT DATA]",
 		description:
-			"AI is transforming the healthcare industry, from improving patient outcomes to streamlining operations. Discover the latest applications of this game-changing technology.",
+			"Prácticas de programación en NTT Data, desarrollando visualizaciones para el análisis de datos en el sector SAE.",
 		style: ``,
 		keywords: [
 			"Artificial Intelligence in Healthcare",
@@ -56,11 +39,12 @@ function article_2() {
 		],
 		body: (
 			<React.Fragment>
-				<h1>Content of article 2</h1>
+				<h1>HOLA???? of article 2</h1>
 			</React.Fragment>
 		),
 	};
 }
+
 
 const myArticles = [article_1, article_2];
 
