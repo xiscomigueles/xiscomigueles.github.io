84864fad6dbf56b84999eb4e5fe2a3bcfd2d9e65
Francisco Javier Migueles Dominguez <miguelesdominguez@gmail.com>
2025-08-24 13:01:13 +0200

    Cambios varios responsive

--- src/components/homepage/otherStudies.jsx
+++ src/components/homepage/otherStudies.jsx
@@ -3,23 +3,17 @@ import { faBook } from "@fortawesome/free-solid-svg-icons";
 
 import Card from "../common/card";
 
-import "./styles/otherStudies.css"
+import "./styles/otherStudies.css";
 import LanguageContext from "../../LanguageContext";
 import translations from "../../data/translations";
 
-
-
 const OtherStudies = () => {
-
 	const { language } = useContext(LanguageContext);
-
 	const [INFO, setINFO] = useState(translations[language]);
-
 
 	useEffect(() => {
 		setINFO(translations[language]);
 	}, [language]);
-
 
 	return (
 		<div className="other-studies">
@@ -28,8 +22,8 @@ const OtherStudies = () => {
 				title={INFO.card_headers.other_studies}
 				body={
 					<div className="other-studies-body">
-						{INFO.other_studies && INFO.other_studies.map(
-							(other_study, index) => (
+						{INFO.other_studies &&
+							INFO.other_studies.map((other_study, index) => (
 								<div className="other-study" key={other_study.id || index}>
 									<img
 										height={30}
@@ -37,15 +31,13 @@ const OtherStudies = () => {
 										alt={`${other_study.title}-study`}
 										className="other-study-image"
 									/>
-									<div className="other-study-title">{other_study.title}</div>
-									<div className="other-study-subtitle">
-										{other_study.institution}
+									<div className="other-study-info">
+										<div className="other-study-title">{other_study.title}</div>
+										<div className="other-study-subtitle">{other_study.institution}</div>
 									</div>
 									<div className="other-study-duration">{other_study.timeframe}</div>
 								</div>
-							)
-						)}
-
+							))}
 					</div>
 				}
 			/>
--- src/components/homepage/studies.jsx
+++ src/components/homepage/studies.jsx
@@ -8,19 +8,13 @@ import "./styles/studies.css";
 import LanguageContext from "../../LanguageContext";
 import translations from "../../data/translations";
 
-
-
 const Studies = () => {
-
 	const { language } = useContext(LanguageContext);
-
 	const [INFO, setINFO] = useState(translations[language]);
-
 
 	useEffect(() => {
 		setINFO(translations[language]);
 	}, [language]);
-
 
 	return (
 		<div className="studies">
@@ -29,20 +23,22 @@ const Studies = () => {
 				title={INFO.card_headers.studies}
 				body={
 					<div className="studies-body">
-						{INFO.studies && INFO.studies.map((study, index) => (
-							<div className="study" key={study.id || index}>
-								<img
-									height={30}
-									src={study.logo}
-									alt={`${study.title}-study`}
-									className="study-image"
-								/>
-								<div className="study-title">{study.title}</div>
-								<div className="study-subtitle">{study.institution}</div>
-								<div className="study-duration">{study.timeframe}</div>
-							</div>
-						))}
-
+						{INFO.studies &&
+							INFO.studies.map((study, index) => (
+								<div className="study" key={study.id || index}>
+									<img
+										height={30}
+										src={study.logo}
+										alt={`${study.title}-study`}
+										className="study-image"
+									/>
+									<div className="study-info">
+										<div className="study-title">{study.title}</div>
+										<div className="study-subtitle">{study.institution}</div>
+									</div>
+									<div className="study-duration">{study.timeframe}</div>
+								</div>
+							))}
 					</div>
 				}
 			/>
